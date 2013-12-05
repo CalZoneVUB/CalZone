d7236a133c30c7b646885f493066b531aa29c786
Timbo925 <twitters@vub.ac.be>
2013-12-05 18:09:51 +0100

    Update

--- JavaServer/calzone/src/main/java/com/vub/controller/LoginNewAccountController.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/LoginNewAccountController.java
@@ -1,27 +1,38 @@
 package com.vub.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.vub.model.Credentials;
 import com.vub.model.User;
 
-@Controller 
+@Controller
 public class LoginNewAccountController {
-	
-	@RequestMapping(value = "/login/create" , method = RequestMethod.GET)
+
+	@RequestMapping(value = "/login/create", method = RequestMethod.GET)
 	public String showLoginCreate(@ModelAttribute("user") User user) {
 		System.out.println("loginCreateAccount GET");
 		return "loginCreateAccount";
 	}
-	
-	@RequestMapping(value = "/login/create" , method = RequestMethod.POST)
+
+	@RequestMapping(value = "/login/create", method = RequestMethod.POST)
 	public String processLoginCreate(@ModelAttribute("user") User user) {
 		System.out.println("loginCreateAccount POST");
-		System.out.println("Asking to create new user with credentials: " + user);
-		return "redirect:user.html";
+		System.out.println("Asking to create new user with credentials: "
+				+ user);
+		return "redirect:/login/create/succesfull/" + user.getName();
+	}
+
+	@RequestMapping("/login/succesfull")
+	public String succesfullLogin() {
+		System.out.println("/login/succesfull");
+		return "succesfullLogin";
+	}
+
+	@RequestMapping("/login/create/succesfull")
+	public String succesfullCreation() {
+		System.out.println("/login/create/succesfull");
+		return "succesfullCreationAccount";
 	}
 }
---
+++ JavaServer/calzone/src/main/webapp/WEB-INF/jsp/succesfullCreationAccount.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>You just created a new account. Look into your mailbox for a confirmation email</h1>
+</body>
+</html>
