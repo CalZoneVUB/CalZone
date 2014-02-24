be65144c4a373e169118248ad2e5558eeaea7b62
CHRISTOPHE GAETHOFS <christophe.gaethofs@trevo.be>
2014-02-24 23:59:17 +0100

    Requestmapping naar /hello

--- JavaServer/calzone/src/main/java/com/vub/controller/HelloController.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class HelloController {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/hello")
 	public String sayHello(Model model) {
 		
 		model.addAttribute("greeting", "Hello World");
