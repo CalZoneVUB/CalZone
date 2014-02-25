3633ede4e5b684ece9ecfe94ae7c0a89a370beb3
Timbo925 <twitters@vub.ac.be>
2014-02-25 02:37:04 +0100

    Activation

--- JavaServer/calzone/src/main/java/com/vub/controller/ActivateAccountController.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/ActivateAccountController.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.vub.model.ActivationKey;
 import com.vub.model.ActivationKeyDao;
-import com.vub.model.SessionDao;
 //import com.vub.model.Credentials;
 import com.vub.model.User;
 import com.vub.model.UserDao;
