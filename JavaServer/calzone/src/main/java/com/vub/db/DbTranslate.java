3e2eb28d8c9d03f9cfd98fe90e63a0963c78bd3f
Pieter Meiresone <pieter.meiresone@gmail.com>
2014-02-28 04:51:33 +0100

    Bugfixes door merge.

--- JavaServer/calzone/src/main/java/com/vub/db/DbTranslate.java
+++ JavaServer/calzone/src/main/java/com/vub/db/DbTranslate.java
@@ -351,7 +351,7 @@ public class DbTranslate {
 				user.setLastName(rs.getString(4));
 				user.setFirstName(rs.getString(5));
 				user.setEmail(rs.getString(6));
-				user.setBirthdate(rs.getDate(7));
+				user.setBirthdate(new java.util.Date(rs.getDate(7).getTime()));
 
 				if (Globals.DEBUG == 1) 
 					System.out.println(user);
--- JavaServer/calzone/src/main/java/com/vub/model/User.java
+++ JavaServer/calzone/src/main/java/com/vub/model/User.java
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 package com.vub.model;
 
-import java.sql.Date;
+import java.util.Date;
 
-//import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
-//import org.hibernate.validator.constraints.URL;
-//import org.springframework.beans.factory.annotation.Value;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.vub.validators.ValidEmail;
 import com.vub.validators.ValidUserName;
@@ -38,11 +37,11 @@ public class User {
 		// setUserName("Calzone");
 		// setPassword("monkey123");
 		setLanguage("NL");
-		setUserTypeName("Extern");
+		setUserTypeName("ROLE_STUDENT");
 		// setFirstName("Bob");
 		// setLastName("Alice");
 		// setEmail("Bob@gmail.com");
-		setBirthdate(Date.valueOf("2000-01-01"));
+		setBirthdate(new Date(2000, 1, 1));
 	}
 
 	public User(String userName) {
@@ -137,130 +136,3 @@ public class User {
 	}
 
 }
-=======
-package com.vub.model;
-
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.URL;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.vub.validators.ValidEmail;
-import com.vub.validators.ValidUserName;
-import com.vub.model.Globals;
-
-public class User {
-	@NotBlank(message="Cannot be empty")
-	@ValidUserName(message = "Username already exists")
-	private String userName;
-	@NotBlank(message="Cannot be empty")
-	@Size(min=8, max=42, message = "Pick a password between 8-42 characters")
-	private String password;
-	private String language;
-	private String userTypeName;
-	@NotBlank(message="Cannot be empty")
-	private String lastName;
-	@NotBlank(message="Cannot be empty")
-	private String firstName;
-	@NotBlank(message="Cannot be empty")
-	@Email(message = "Not a real email adress")
-	@ValidEmail(message = "Email already exist.")
-	private String email;
-	private Date birthdate;
-
-	public User() {
-		// setUserName("Calzone");
-		// setPassword("monkey123");
-		setLanguage("NL");
-		setUserTypeName("ROLE_STUDENT");
-		// setFirstName("Bob");
-		// setLastName("Alice");
-		// setEmail("Bob@gmail.com");
-		setBirthdate(new Date(2000, 1, 1));
-	}
-
-	public User(String userName) {
-		setUserName(userName);
-		if (Globals.DEBUG == 1) 
-			System.out.println("Created User with: " + userName);
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getLanguage() {
-		return language;
-	}
-
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-
-	public String getUserTypeName() {
-		return userTypeName;
-	}
-
-	public void setUserTypeName(String userTypeName) {
-		this.userTypeName = userTypeName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getBirthdate() {
-		return birthdate;
-	}
-
-	public void setBirthdate(Date birthdate) {
-		this.birthdate = birthdate;
-	}
-
-	@Override
-	public String toString() {
-		return "User [userName=" + userName + ", password=" + password
-				+ ", language=" + language + ", userTypeName=" + userTypeName
-				+ ", lastName=" + lastName + ", firstName=" + firstName
-				+ ", email=" + email + ", birthdate=" + birthdate + "]";
-	}
-
-}
->>>>>>> Dev
