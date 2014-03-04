36aaa278e730be5c19da87223e1f842d7334aca6
Timbo925 <twitters@vub.ac.be>
2014-03-04 05:39:30 +0100

    test3

--- JavaServer/calzone/src/main/java/com/vub/controller/Test3.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/Test3.java
@@ -1,14 +1,28 @@
 package com.vub.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.vub.dao.RoomDao;
+import com.vub.datadump.ReadCSV;
+import com.vub.model.Room;
 
 @Controller 
 public class Test3 {
 	@RequestMapping(value = "/test3")
 	public String sayHello(Model model) {
-		model.addAttribute("greeting", "Hello World");
+		
+		ReadCSV csv = new ReadCSV();
+		ArrayList<Room> roomList = csv.readRoom("Leslokalen.csv",";");
+		RoomDao roomDao = new RoomDao();
+		
+		for (Room room : roomList) {
+			roomDao.insertRoom(room);
+		}
+
 		return "hello";
 	}
 }
