308aade0b74d51b045eb5aea3e36d7a917d32eeb
Sam <svdvonde@vub.ac.be>
2014-03-11 23:04:32 +0100

    Toevoegen/Editen van classrooms werkt (nog geen mooie CSS opmaak)

--- JavaServer/calzone/src/main/java/com/vub/controller/ClassroomsController.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/ClassroomsController.java
@@ -17,22 +17,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.vub.dao.RoomDao;
 import com.vub.model.Globals;
 import com.vub.model.Room;
+import com.vub.model.RoomType;
 import com.vub.validators.ClassroomValidator;
 
 @Controller 
 public class ClassroomsController {
 	
-	@RequestMapping(value = "/classrooms/", method = RequestMethod.GET)
+	@RequestMapping(value = "/classrooms", method = RequestMethod.GET)
 	public String mainPage(Model model) {
 		RoomDao roomDao = new RoomDao();
 		ArrayList<Room> classroomArrayList = roomDao.getRooms();
+		
 		model.addAttribute("room", new Room());
+		model.addAttribute("editWindow", false);
 		model.addAttribute("classroomArrayList", classroomArrayList);
-		return "Classrooms"; 
-	}
-	
-	@RequestMapping(value = "/classrooms/edit-{id}/", method = RequestMethod.GET)
-	public String getRooms(Model model, @PathVariable int id) {
+		model.addAttribute("roomTypes", RoomType.values());
+		roomDao.closeDao();
 		return "Classrooms"; 
 	}
 	
@@ -50,9 +50,59 @@ public class ClassroomsController {
 				if (Globals.DEBUG == 1)
 					System.out.println(error);
 			return "Classrooms";
+		} else {
+			RoomDao roomDao = new RoomDao();
+			roomDao.insertRoom(room);
+			System.out.println(room.getType());
+			roomDao.closeDao();
+			return "redirect:/classrooms";
 		}
+	}
+	
+	@RequestMapping(value = "/classrooms/edit-{id}", method = RequestMethod.GET)
+	public String editPagePost(Model model, @PathVariable int id) {
+		RoomDao roomDao = new RoomDao();
+		ArrayList<Room> classroomArrayList = roomDao.getRooms();
+		
+		model.addAttribute("room", getRoom(id, classroomArrayList));
+		model.addAttribute("editWindow", true);
+		model.addAttribute("classroomArrayList", classroomArrayList);
+		model.addAttribute("roomTypes", RoomType.values());
+		roomDao.closeDao();
+		return "Classrooms"; 
+	}
+	
+	@RequestMapping(value = "/classrooms/edit-{id}", method = RequestMethod.POST)
+	public String editPage(Model model, @ModelAttribute("room") Room room, BindingResult result) {
+		editRoom(room);
+		return "redirect:/classrooms"; 
+	}
+	
+	// TODO - it is more efficient to let the database handle the selection procedure
+	private Room getRoom(int id, ArrayList<Room> classroomArrayList) {
+		for(Room r : classroomArrayList) {
+			if(r.getRoomId() == id)
+				return r;
+		}
+		return null;
+	}
+	
+	// TODO -- This is where rooms get edited!
+	private void editRoom(Room room) {
+		RoomDao roomDao = new RoomDao();
+		Room r = getRoom(room.getRoomId(), roomDao.getRooms());
+		if(r == null)
+			roomDao.insertRoom(room);
 		else {
-			return "Classrooms";
+			r.setBuilding(room.getBuilding());
+			r.setFloor(room.getFloor());
+			r.setName(room.getName());
+			r.setCapacity(room.getCapacity());
+			r.setProjectorEquipped(room.isProjectorEquipped());
+			r.setRecorderEquipped(room.isRecorderEquipped());
+			r.setSmartBoardEquipped(room.isSmartBoardEquipped());
+			roomDao.insertRoom(r);
 		}
+		roomDao.closeDao();
 	}
 }
