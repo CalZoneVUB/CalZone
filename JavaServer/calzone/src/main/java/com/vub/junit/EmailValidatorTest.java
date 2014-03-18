1f70763f34f15abb357376b52e4fa8ba7cd41959
Timbo925 <twitters@vub.ac.be>
2014-03-18 15:37:03 +0100

    Email Test

---
+++ JavaServer/calzone/src/main/java/com/vub/junit/EmailValidatorTest.java
@@ -0,0 +1,14 @@
+package com.vub.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmailValidatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
