8767422077f41da2185fea4b931394f07295c0e3
Sam <svdvonde@vub.ac.be>
2014-03-24 23:32:07 +0100

    [NIET WERKEND] Basismodel courses en de database operaties

--- JavaServer/calzone/src/main/java/com/vub/model/Program.java
+++ JavaServer/calzone/src/main/java/com/vub/model/Program.java
@@ -4,39 +4,28 @@
  */
 package com.vub.model;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Program {
-	String iD;
-	String faculty;
-	String department;
-	ArrayList<Course> listOfCourses;
+	@Id
+	@GeneratedValue
+	@Column(name="ProgramID")
+	int id;
 	
-	public Program() {
-		listOfCourses = new ArrayList<Course>();
-	}
-
-	public String getiD() {
-		return iD;
-	}
-
-	public void setiD(String iD) {
-		this.iD = iD;
-	}
-
-	public String getFaculty() {
-		return faculty;
-	}
-
-	public void setFaculty(String faculty) {
-		this.faculty = faculty;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "FacultyID")
+	private Faculty faculty;
+	
+	@OneToMany(mappedBy="program")
+	private List<CourseProgramAssociation> courses;
 }
