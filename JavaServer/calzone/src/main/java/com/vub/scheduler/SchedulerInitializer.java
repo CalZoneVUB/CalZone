bb9210d3454c423da6ac237d2d0bd55ada188bae
Pieter Meiresone <pieter.meiresone@gmail.com>
2014-04-01 00:30:38 +0200

    SchedulerInitializer.java

---
+++ JavaServer/calzone/src/main/java/com/vub/scheduler/SchedulerInitializer.java
@@ -0,0 +1,24 @@
+package com.vub.scheduler;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Class that contains static methods for initializing data for the scheduler.
+ * Fetches the data from the database.
+ * 
+ * @author pieter
+ *
+ */
+public class SchedulerInitializer {
+	
+	/**
+	 * Creates all the correct date time slots for a given academic year. 
+	 * This method takes into account bloc period.
+	 * @param yearStart
+	 * @return
+	 */
+	public static List<Date> createDateList(int yearStart) {
+		
+	}
+}
