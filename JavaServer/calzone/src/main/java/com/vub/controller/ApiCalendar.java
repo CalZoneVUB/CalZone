8775f22c7444fa2b4efaa1bee9712506acda2378
Timbo925 <twitters@vub.ac.be>
2014-04-03 20:13:38 +0200

    API Calendar Example

--- JavaServer/calzone/src/main/java/com/vub/controller/ApiCalendar.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/ApiCalendar.java
@@ -2,6 +2,7 @@ package com.vub.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.vub.model.Course;
+import com.vub.model.CourseComponent;
 import com.vub.model.Entry;
 import com.vub.model.Room;
 
@@ -31,21 +34,48 @@ public class ApiCalendar {
 	 */
 	@RequestMapping(value = "{type}/{id}/{week}", method = RequestMethod.GET)
     @ResponseBody
-    public Room test(@PathVariable String type, @PathVariable int id, @PathVariable int week) throws ParseException {
+    public ArrayList<Entry>  test(@PathVariable String type, @PathVariable int id, @PathVariable int week) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
 		
+		ArrayList<Entry> list = new ArrayList<Entry>();
+		
+		Entry entry = new Entry();
 		Room room = new Room();
 		room.setCapacity(100);
 		room.setProjectorEquipped(true);
-		room.setRecorderEquipped(true);	
-		
-		Entry entry = new Entry();
+		room.setRecorderEquipped(true);
+		room.setDisplayName("F.4.185");
 		Date d1 = sdf.parse("04-04-2014 14:00:00");
 		entry.setStartDate(d1);
 		Date d2 = sdf.parse("04-04-2014 16:00:00");
 		entry.setEndDate(d2);
-
-        return room;
+		Course c1 = new Course();
+		entry.setRoom(room);
+		c1.setCourseName("Kansrekening I - TEST");
+		CourseComponent cc1 = new CourseComponent();
+		cc1.setCourse(c1);
+		
+		list.add(entry);
+		
+		Entry entry2 = new Entry();
+		Room room2 = new Room();
+		room2.setCapacity(200);
+		room2.setProjectorEquipped(true);
+		room2.setRecorderEquipped(true);
+		room2.setDisplayName("Q.A. Test");
+		Date d3 = sdf.parse("05-04-2014 14:00:00");
+		entry2.setStartDate(d3);
+		Date d4 = sdf.parse("05-04-2014 16:00:00");
+		entry2.setEndDate(d4);
+		Course c2 = new Course();
+		c2.setCourseName("Kansrekening I - TEST");
+		CourseComponent cc2 = new CourseComponent();
+		cc2.setCourse(c2);
+		entry2.setRoom(room2);
+		
+		list.add(entry2);
+		
+        return list;
     }
 	
 	/**
