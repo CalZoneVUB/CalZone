bef144ef570891d85fb3790ffc7f8e9cd91bfe4d
Sam <svdvonde@vub.ac.be>
2014-04-11 17:03:08 +0200

    Opstellen test folder

---
+++ JavaServer/calzone/src/main/test/com/vub/service/KeyServiceTests.java
@@ -0,0 +1,14 @@
+package com.vub.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KeyServiceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
