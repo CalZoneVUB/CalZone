5a80cbe5477589b19147fd70dab0006611709e3a
Timbo925 <twitters@vub.ac.be>
2014-04-28 22:04:00 +0200

    Course Add working

--- JavaServer/calzone/src/main/java/com/vub/controller/ApiCourse.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/ApiCourse.java
@@ -1,10 +1,10 @@
 package com.vub.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.eclipse.jdt.internal.compiler.flow.FinallyFlowContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,15 +18,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.vub.exception.CourseComponentNotFoundException;
 import com.vub.exception.CourseNotFoundException;
 import com.vub.model.Course;
 import com.vub.model.CourseComponent;
 import com.vub.model.CourseComponent.CourseComponentTerm;
 import com.vub.model.CourseComponent.CourseComponentType;
+import com.vub.model.CourseData;
 import com.vub.model.JsonResponse;
 import com.vub.model.SelectResponseConverter;
 import com.vub.model.XeditCheckbox;
+import com.vub.scheduler.SchedulerInitializer;
 import com.vub.service.CourseComponentService;
 import com.vub.service.CourseService;
 
@@ -57,13 +61,13 @@ public class ApiCourse {
 		context.close();
 		return listSelectResponses;
 	}
-	
+
 	@RequestMapping(value="/api/course/delete/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public JsonResponse deleteCourse(@PathVariable int id) {		
 		final Logger logger = LoggerFactory.getLogger(this.getClass());
 		logger.info("Deliting Coruse with id: " + id);
-		
+
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		CourseService courseService = (CourseService) context.getBean("courseService");
 		JsonResponse jsonResponse = new JsonResponse();
@@ -76,14 +80,14 @@ public class ApiCourse {
 			jsonResponse.setStatus("error");
 			jsonResponse.setMessage("Course not found with id: " + id);
 		} 		
-		
+
 		jsonResponse.setStatus("success");
 		jsonResponse.setMessage("OK");
-		
+
 		context.close();
 		return jsonResponse;
 	}
-	
+
 	/**
 	 * @param value : value input by the user
 	 * @param name : name given by the id in javascript
@@ -222,4 +226,91 @@ public class ApiCourse {
 
 		return json;
 	}
+
+	@RequestMapping(value="/api/course/new", method = RequestMethod.POST)
+	@ResponseBody
+	public JsonResponse testPost(@RequestBody String string) {
+		System.out.println(string);
+
+		JsonResponse jsonResponse = new JsonResponse();
+		JsonParser parser = new JsonParser();
+		JsonObject json = (JsonObject) parser.parse(string);
+		Course course = new Course();
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CourseService courseService = (CourseService) context.getBean("courseService");
+		CourseComponentService componentService = (CourseComponentService) context.getBean("courseComponentService");
+
+		int courseComponentCount = ((json.entrySet().size()) - 7) /  8; //Returns the amount of coursecomponent are in the request
+		System.out.println("Amount CourseComponent: " + courseComponentCount);
+
+		course.setCourseName(json.get("new_courseName").getAsString());
+		CourseData courseData = new CourseData();
+		course.setCourseData(courseData);
+		course.getCourseData().setECTS(json.get("new_ECTS").getAsInt());
+		course.getCourseData().setStudyTime(json.get("new_studyTime").getAsInt());
+		switch ((json.get("new_reexamination").getAsInt())) {
+		case 0:
+			course.getCourseData().setReexaminationPossible(false); break;
+		case 1:
+			course.getCourseData().setReexaminationPossible(true); break;
+		}
+		switch (json.get("new_language").getAsInt()) {
+		case 0:
+			course.getCourseData().setLanguage("BE_NL"); break;
+		case 1:
+			course.getCourseData().setLanguage("UK_ENG"); break;
+		}
+		course.getCourseData().setLearningGoals(json.get("new_results").getAsString());
+		course.getCourseData().setGrading(json.get("new_grading").getAsString());
+		List<CourseComponent> components = new ArrayList<>();
+		if (courseComponentCount > 0 ) {
+			for (int i = 1; i < courseComponentCount+1; i++) {
+				CourseComponent cc = new CourseComponent();
+				switch (json.get("new_courseComponent" + i).getAsInt()) {
+				case 0:
+					cc.setType(CourseComponentType.HOC); break;
+				case 1:
+					cc.setType(CourseComponentType.WPO); break;
+				case 2:
+					cc.setType(CourseComponentType.EXM); break;
+				case 3: 
+					cc.setType(CourseComponentType.ZLF); break;
+				}
+				switch (json.get("new_semester" + i).getAsInt()) {
+				case 0:
+					cc.setTerm(CourseComponentTerm.EX); break;
+				case 1:
+					cc.setTerm(CourseComponentTerm.S1); break;
+				case 2:
+					cc.setTerm(CourseComponentTerm.S2); break;
+				case 3: 
+					cc.setTerm(CourseComponentTerm.S3); break;
+				}
+				cc.setContactHours(json.get("new_contactHours" + i).getAsInt());
+				cc.setStartingDate(SchedulerInitializer.createSlotsOfWeek(2014, json.get("new_startDate" + i).getAsInt()).get(0));
+				cc.setEndingDate(SchedulerInitializer.createSlotsOfWeek(2014, json.get("new_endDate" + i).getAsInt()).get(0));
+				cc.setDuration(json.get("new_duration" + i).getAsInt());
+				cc.setRoomCapacityRequirement(json.get("new_roomCapacity" + i).getAsInt());
+				
+				//TODO include projector 
+				components.add(cc);
+				System.out.println("CC");
+				cc.setCourse(course);
+				
+			}
+			
+			System.out.println(components);
+			//course.setCourseComponents(components);
+			jsonResponse.setMessage(course.toString());
+			jsonResponse.setStatus(JsonResponse.SUCCESS);
+			
+			courseService.updateCourse(course);
+			for (CourseComponent c : components) {
+				componentService.updateCourseComponent(c);
+			}
+			context.close();
+		}
+		return jsonResponse;
+	}
+
 }
--- JavaServer/calzone/src/main/java/com/vub/controller/HelloController.java
+++ JavaServer/calzone/src/main/java/com/vub/controller/HelloController.java
@@ -1,14 +1,26 @@
 package com.vub.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.vub.model.Entry;
+import com.vub.model.Room;
 import com.vub.model.Traject;
+import com.vub.scheduler.Schedular;
+import com.vub.scheduler.SchedularSolver;
+import com.vub.scheduler.SchedulerInitializer;
+import com.vub.service.EntryService;
+import com.vub.service.RoomService;
 import com.vub.service.TrajectService;
-import com.vub.service.UserService;
 
 @Controller 
 public class HelloController {
@@ -17,13 +29,28 @@ public class HelloController {
 		model.addAttribute("greeting", "Hello World");
 		
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
 		TrajectService trajectService = (TrajectService) context.getBean("trajectService");
+		RoomService roomService = (RoomService) context.getBean("roomService");
+		EntryService entryService = (EntryService) context.getBean("entryService");
 		
-		Traject traject = trajectService.findTrajectByIdInitializedFull(3);
-		System.out.println("Hallo controller: " + traject.getCourses().iterator().next().getCourseComponents());
+		List<Room> roomsList = new ArrayList<Room>();
+		roomsList.addAll(roomService.getRooms());
+	
+		Set<Traject> trajects = new HashSet<Traject>();
+		trajects.add(trajectService.findTrajectByIdInitializedFull(177));
 		
+		List<Date> dateSlots = SchedulerInitializer.createSlotsOfWeek(2014, 5);
+		dateSlots.addAll(SchedulerInitializer.createSlotsOfWeek(2014, 6));
+
+		SchedularSolver schedularSolver = new SchedularSolver(dateSlots, roomsList, trajects);
+		Schedular schedular = schedularSolver.run();
+		
+		for (Entry e: schedular.getEntryList()) {
+			entryService.updateEntry(e);
+			System.out.println("Schedule: "  + e);
+		}
 		context.close();
+		
 		return "hello";
 	}
 }
