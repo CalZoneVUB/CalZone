7907b37e5157bd81338a784a82ab8cf467f99874
Pieter Meiresone <pieter.meiresone@gmail.com>
2014-05-21 03:42:29 +0200

    Snelle fix voor teacherLecturePreference.java

--- JavaServer/calzone/src/main/java/com/vub/scheduler/constraints/TeacherLecturePreferenceCV.java
+++ JavaServer/calzone/src/main/java/com/vub/scheduler/constraints/TeacherLecturePreferenceCV.java
@@ -20,26 +20,27 @@ public class TeacherLecturePreferenceCV implements ConstraintViolation {
 	@Override
 	public String description() {
 		// TODO Internationalize
-		String msg = "Course ";
-		try {
-		msg += entry.getCourseComponent().getCourse().getCourseName();
-		msg += " has a prefered day of ";
-		msg += DateUtility.getDayOfWeek(entry.getCourseComponent()
-				.getTeacherLecturePreference().getDayOfWeek());
-		msg += ", a prefered starting hour of ";
-		msg += entry.getCourseComponent().getTeacherLecturePreference().getStartingHour();
-		msg += ":00, a prefered ending hour of ";
-		msg += entry.getCourseComponent().getTeacherLecturePreference().getEndingHour();
-		msg += ":00 but the course is given at ";
-		msg += DateUtility.getDayOfWeek(DateUtility.getDayOfWeek(entry.getStartingDate()));
-		msg += " between ";
-		msg += DateUtility.getHourOfDay(entry.getStartingDate());
-		msg += ":00 and ";
-		msg += DateUtility.getHourOfDay(entry.getEndingDate());
-		msg += ":00.";
-		} catch (Exception e) {
-			return "" ;
-		}
-		return msg;
+//		String msg = "Course ";
+//		try {
+//		msg += entry.getCourseComponent().getCourse().getCourseName();
+//		msg += " has a prefered day of ";
+//		msg += DateUtility.getDayOfWeek(entry.getCourseComponent()
+//				.getTeacherLecturePreference().getDayOfWeek());
+//		msg += ", a prefered starting hour of ";
+//		msg += entry.getCourseComponent().getTeacherLecturePreference().getStartingHour();
+//		msg += ":00, a prefered ending hour of ";
+//		msg += entry.getCourseComponent().getTeacherLecturePreference().getEndingHour();
+//		msg += ":00 but the course is given at ";
+//		msg += DateUtility.getDayOfWeek(DateUtility.getDayOfWeek(entry.getStartingDate()));
+//		msg += " between ";
+//		msg += DateUtility.getHourOfDay(entry.getStartingDate());
+//		msg += ":00 and ";
+//		msg += DateUtility.getHourOfDay(entry.getEndingDate());
+//		msg += ":00.";
+//		} catch (Exception e) {
+//			return "" ;
+//		}
+//		return msg;
+		return "";
 	}
 }
